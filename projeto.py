4f743777a6619ec9c779b025b1bf5ecaa49cc75a
Terres06 <pedrocoutoterres@gmail.com>
2025-06-07 00:05:19 -0300

    funcoes matrizes implementadas + plateia.txt

--- projeto.py
+++ projeto.py
@@ -1,4 +1,6 @@
-import sys 
+import os 
+
+ARQUIVO = "plateia.txt"
 
 def menu():
     print("-------------------------")
@@ -21,12 +23,12 @@ def validaOpcao (opcao, min, max):
         print("Valor inválido, digite um número!")
         return escolhe_opcao()
 
-        
-
 def escolhe_opcao():
-    opcao = input("Digite a opção: ")
-    validaOpcao(opcao, 0, 4)
+
     while True:
+        opcao = input("Digite a opção: ")
+        opcao = validaOpcao(opcao,0,4)    
+
         if opcao == 1:
             mostra_plateia()
             menu()
@@ -35,19 +37,45 @@ def escolhe_opcao():
             menu()
         elif opcao == 3:
             print()
-            menu
+            menu()
         elif opcao == 4:
             print()
             break
         elif opcao == 0:
-            sys.exit("Encerrando programa....")
+            os.system('cls')
+            print("Saindo do programa....")
             break
-    
 
+def carregar_dados (nome_arquivo):
+    if os.path.exists(nome_arquivo):
+        arq = open(nome_arquivo, 'r')
+        dados = []
+        for linha in arq:
+            dados.append(list(linha.strip()))
+        arq.close()
+        return dados
+    else:
+        return [['-' for j in range (12)] for i in range (10)]
+    
+    
+def salvar_dados(nome_arquivo, dados):
+    arq = open(nome_arquivo, 'w')
+    for linha in dados:
+        arq.write("".join(linha) + '\n')
+    arq.close()
+    print("Matriz salva em: ", nome_arquivo)
+
+def exibir_matriz(matriz):
+    print("\n Matriz atual:")
+    for linha in matriz:
+        print(" ".join(linha))
+    print()
 
 def mostra_plateia():
-    print("Deu boa")
-    return 0
+    matriz = carregar_dados(ARQUIVO)
+    exibir_matriz(matriz)
+    salvar_dados(ARQUIVO, matriz)
+
 def mostra_ocupacao():
     print()
 def vender_ingresso():
